360701383eb8254086f26fd264ececf578785b96
simyeekai <simyeekai@gmail.com>
2025-06-01 00:53:31 +0800

    hi

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages & components
+import Home from "./pages/home/home";
+import Login from "./pages/login/login";
+import Signup from "./pages/signup/signup";
+
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ finance-tracker/src/pages/home/home.js
@@ -0,0 +1,10 @@
+import styles from './home.module.css'
+import { View, Text} from "react-native"
+
+export default function Home() {
+    return (
+        <view>
+            <text> home </text>
+        </view>
+    )
+}
