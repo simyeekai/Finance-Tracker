b0048c6089dc52922cd44f21e82a9f22c310f985
simyeekai <simyeekai@gmail.com>
2025-06-07 23:43:53 +0800

    signup hook 2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,19 @@
 import firebase from "firebase/compat/app";
-import 'firebase/firestore'
-import 'firebase/auth'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA4OppwSz93OIQ2ALTWs493ir0IyOYJY-A",
-    authDomain: "finance-tracker-8bf32.firebaseapp.com",
-    projectId: "finance-tracker-8bf32",
-    storageBucket: "finance-tracker-8bf32.firebasestorage.app",
-    messagingSenderId: "217152590793",
-    appId: "1:217152590793:web:bdfbcb7470e889da760e18"
-  };
+  apiKey: "AIzaSyA4OppwSz93OIQ2ALTWs493ir0IyOYJY-A",
+  authDomain: "finance-tracker-8bf32.firebaseapp.com",
+  projectId: "finance-tracker-8bf32",
+  storageBucket: "finance-tracker-8bf32.firebasestorage.app",
+  messagingSenderId: "217152590793",
+  appId: "1:217152590793:web:bdfbcb7470e889da760e18"
+};
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const projectFirestore = firebase.firestore()
-const projectAuth = firebase.auth()
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-export { projectFirestore, projectAuth }
+export { projectFirestore, projectAuth };
--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup';
 
 import styles from './signup.module.css'
 
@@ -6,10 +7,11 @@ export default function Signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [displayName, setDisplayName] = useState('');
+    const { signup, isPending, error } = useSignup()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(email, password, displayName)
+        signup(email, password, displayName)
     }
 
     return (
@@ -41,7 +43,9 @@ export default function Signup() {
                     value = {displayName}
                 />
             </label>
-            <button className='btn'>Signup</button>
+            {!isPending && <button className='btn'>Signup</button>}
+            {isPending && <button className='btn' disabled>loading</button>}
+            {error && <p>{error}</p>}
         </form>
     )
 }
