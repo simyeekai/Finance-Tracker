5c7f420b50616d825c2fb1d6101bdb7981ddaad8
simyeekai <simyeekai@gmail.com>
2025-06-16 22:01:55 +0800

    transactionForm

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,15 @@
 import styles from './home.module.css';
+import TransactionForm from './transactionForm';
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className={styles.container}>
+            <div class={styles.content}>
+                transaction list
+            </div>
+            <div className={styles.sidebar}>
+                <TransactionForm />
+            </div>
         </div>
     )
 }
