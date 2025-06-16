1c3bfeabdd8a04801bc0963bbe96c8858a0d47d5
simyeekai <simyeekai@gmail.com>
2025-06-17 00:37:20 +0800

    useCollection

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,29 @@
+import { useEffect, useState, useRef } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useCollection = (collection, _query, _orderBy) => {
+  const [documents, setDocuments] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection)
+
+    const unsubscribe = ref.onSnapshot(snapshot => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({...doc.data(), id: doc.id})
+      });
+      
+      setDocuments(results)
+      setError(null)
+    }, error => {
+      console.log(error)
+      setError('could not fetch the data')
+    })
+
+    return () => unsubscribe()
+
+  }, [collection])
+
+  return { documents, error }
+}
