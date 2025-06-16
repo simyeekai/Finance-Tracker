2ebbc5c8b442b1f517bcc0c2c5e2bef61b411245
simyeekai <simyeekai@gmail.com>
2025-06-17 00:50:45 +0800

    transactionListed

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,19 @@
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection';
 
 import styles from './home.module.css';
 import TransactionForm from './transactionForm';
+import TransactionList from './transactionList';
 
 export default function Home() {
     const { user } = useAuthContext()
+    const { documents, error } = useCollection('transactions')
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                transaction list
+                {error && <p>{error}</p>}
+                {documents && <TransactionList transactions={documents}/>}
             </div>
             <div className={styles.sidebar}>
                 <TransactionForm uid={user.uid}/>
