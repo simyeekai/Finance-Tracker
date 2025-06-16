6f82039a3cc75d622119e3705203c55d57fa1158
simyeekai <simyeekai@gmail.com>
2025-06-16 22:20:16 +0800

    boilerplate

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,42 @@
+import { useReducer, useEffect, useState, useSyncExternalStore } from "react";
+import { projectFireStore } from "../firebase/config";
+
+let initialState = {
+    document: null,
+    isPending: false,
+    error: null,
+    success: null
+}
+
+const firestoreReducer = (state, action) => {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
+
+export const useFirestore = (collection) => {
+    const [response, dispatch] = useReducer(firestoreReducer, initialState);
+    const [isCancelled, setIsCancelled] = useState(false)
+
+    // collection  ref
+    const ref = projectFireStore.collection(collection)
+
+    // add document
+    const addDocument = (doc) => {
+
+    }
+
+    // delete document
+    const deleteDocument = (id) => {
+
+    }
+
+    useEffect(() => {
+        return () => setIsCancelled(true)
+    }, [])
+
+    return { addDocument, deleteDocument, response }
+
+}
