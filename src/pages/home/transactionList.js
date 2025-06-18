86075f69772e8f9c6817e525c112d1dffcb0fc99
simyeekai <simyeekai@gmail.com>
2025-06-18 22:10:24 +0800

    deleteDocument

--- src/pages/home/transactionList.js
+++ src/pages/home/transactionList.js
@@ -2,8 +2,9 @@ import { useFirestore } from '../../hooks/useFirestore'
 import styles from './home.module.css'
 
 export default function TransactionList({ transactions }) {
-    const { deleteDocument } = useFirestore('transactions')
-  
+    const { deleteDocument, response } = useFirestore('transactions')
+    console.log(response)
+
     return (
       <ul className={styles.transactions}>
         {transactions.map((transaction) => (
